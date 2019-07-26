d78e33f0214981a94a91f2b031a2012d9a1b1b19
vijaykasar <48680914+vijaykasar@users.noreply.github.com>
2019-07-26 17:46:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var http = require('http');
  
 var port = 8081;
- var a = "Welcome";
 var s = http.createServer();
 s.on('request', function(request, response) {
     response.writeHead(200);
     console.log(request.method);
     console.log(request.headers);
     console.log(request.url);
-    response.write('hi......');
-    response.send(a);
+    response.write('Welcome');
     response.end();
 });
  
